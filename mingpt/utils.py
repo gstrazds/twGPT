3e48770d27782c61b4efa6027c1749c2e25f2c78
G Strazds <gstrazds@hotmail.com>
2021-06-17 14:04:04 +0300

    Work in progress: toward batched cmd_acc as a validation metric

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -35,21 +35,26 @@ def sample(model, block_size, x, steps, temperature=1.0, sample=False, top_k=Non
     for k in range(steps):
         x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
         logits, _ = model(x_cond)
-        # pluck the logits at the final step and scale by temperature
-        logits = logits[:, -1, :] / temperature
-        # print(f"sample: logits.size = {logits.size()}")
-        # optionally crop logits to only the top k options
-        if top_k is not None:
-            logits = top_k_logits(logits, top_k)
-        # apply softmax to convert to probabilities
-        probs = F.softmax(logits, dim=-1)
-        # sample from the distribution or take the most likely
-        if sample:
-            ix = torch.multinomial(probs, num_samples=1)
-        else:
-            _, ix = torch.topk(probs, k=1, dim=-1)
+        logits = logits[:, -1, :]  # use the logits from the last seq pos
+        ix = tokid_from_logits(logits, temperature=temperature, sample=sample, top_k=top_k)
         # append to the sequence and continue
         x = torch.cat((x, ix), dim=1)
 
     return x
+
+@torch.no_grad()
+def tokid_from_logits(logits, temperature=1.0, sample=False, top_k=None):
+    logits / temperature
+    # print(f"sample: logits.size = {logits.size()}")
+    # optionally crop logits to only the top k options
+    if top_k is not None:
+        logits = top_k_logits(logits, top_k)
+    # apply softmax to convert to probabilities
+    probs = F.softmax(logits, dim=-1)
+    # sample from the distribution or take the most likely
+    if sample:
+        ix = torch.multinomial(probs, num_samples=1)
+    else:
+        _, ix = torch.topk(probs, k=1, dim=-1)
+    return ix
 
